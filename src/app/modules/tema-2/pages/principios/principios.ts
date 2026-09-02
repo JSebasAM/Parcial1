9f877c4c6517975c798a684ed50798962f91145f
stevendz2002 <jhon.loaiza01@uceva.edu.co>
2026-09-02 17:39:01 -0500

    feat(tema-2): generar estructura base para el modulo de principios de arquitectura

---
+++ src/app/modules/tema-2/pages/principios/principios.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principios',
+  standalone: false,
+  templateUrl: './principios.html',
+  styleUrl: './principios.scss',
+})
+export class Principios {
+
+}
