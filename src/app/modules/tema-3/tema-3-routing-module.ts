3b843ec04ce53d80d0ce52a3ace1d06a34c7b4a2
Antonio Jose Ortega Valencia <antonio.ortega01@uceva.edu.co>
2026-09-02 17:59:26 -0500

    feat: creacion de componente design-system, configuracion de ruta y cambios en el html y css

--- src/app/modules/tema-3/tema-3-routing-module.ts
+++ src/app/modules/tema-3/tema-3-routing-module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sistema-diseno',
+    component: DesignSystemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
