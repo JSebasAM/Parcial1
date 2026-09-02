07e1de0e11e07c15754558c2208f9b60cf2653ef
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2026-09-02 17:19:20 -0500

    feature: creación del tema 3

---
+++ src/app/modules/tema-3/tema-3-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Tema3RoutingModule } from './tema-3-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Tema3RoutingModule
+  ]
+})
+export class Tema3Module { }
